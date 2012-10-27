cf5be35d76e6adf020a99d1b2986e5d0a44870b9
yuki <kimaira7@gmail.com>
2012-10-27 21:01:20 +0900

    コピー＆ペースト操作の検出と記録に関して、アプリケーション外からコピーしてきたデータをペーストした際に、以前コピーしたノートと今回ペーストしたノートとの関連性が追加されてしまう問題を修正。

--- src/cx/fbn/nevernote/neighbornote/ClipBoardObserver.java
+++ src/cx/fbn/nevernote/neighbornote/ClipBoardObserver.java
@@ -1,18 +1,35 @@
 package cx.fbn.nevernote.neighbornote;
 
+import com.trolltech.qt.gui.QApplication;
+
 public class ClipBoardObserver {
 	private String SourceGuid;
+	private boolean internalFlg;
 	
 	public ClipBoardObserver(){
-		SourceGuid = new String();
+		SourceGuid = new String("");
+		internalFlg = false;
+		QApplication.clipboard().dataChanged.connect(this, "clipboardDataChanged()");
 	}
 	
 	public void setCopySourceGuid(String guid){
 		SourceGuid = guid;
+		internalFlg = true;
 	}
 	
 	public String getSourceGuid(){
+		if(SourceGuid == ""){
+			return null;
+		}
 		return SourceGuid;
+	}
+	
+	@SuppressWarnings("unused")
+	private void clipboardDataChanged(){
+		if(!internalFlg){
+			SourceGuid = "";
+		}
+		internalFlg = false;
 	}
 
 }
