fe3fc124920eb2956046fd95a7be7826fb074cdd
yuki <kimaira7@gmail.com>
2012-12-04 17:14:50 +0900

    関連ノートリストの最大アイテム表示数を設定できるようにした

--- src/cx/fbn/nevernote/gui/RensoNoteList.java
+++ src/cx/fbn/nevernote/gui/RensoNoteList.java
@@ -123,40 +123,45 @@ public class RensoNoteList extends QListWidget {
 		HashMap<String, Integer> copyHistory = new HashMap<String, Integer>();
 		copyHistory.putAll(History);
 		
-		// 操作回数が多い順に取り出して連想ノートリストに追加
-		while (!copyHistory.isEmpty()) {
-			int maxNum = -1;
-			String maxGuid = new String();
-			Iterator<String> it = copyHistory.keySet().iterator();
-			
-			while (it.hasNext()) {
-				String nextGuid = it.next();
-				int tmpNum = copyHistory.get(nextGuid);
-				if (tmpNum > maxNum) {
-					maxNum = tmpNum;
-					maxGuid = nextGuid;
+		// 連想ノートリストアイテムの最大表示数まで繰り返す
+		for (int i = 0; i < Global.getRensoListItemMaximum(); i++) {
+			// 操作回数が多い順に取り出して連想ノートリストに追加
+			if (!copyHistory.isEmpty()) {
+				int maxNum = -1;
+				String maxGuid = new String();
+				Iterator<String> it = copyHistory.keySet().iterator();
+				
+				while (it.hasNext()) {
+					String nextGuid = it.next();
+					int tmpNum = copyHistory.get(nextGuid);
+					if (tmpNum > maxNum) {
+						maxNum = tmpNum;
+						maxGuid = nextGuid;
+					}
 				}
-			}
-			
-			// 次の最大値探索で邪魔なので最大値をHashMapから削除
-			copyHistory.remove(maxGuid);
-
-			// 関連度最大のノートがアクティブか確認
-			Note maxNote = conn.getNoteTable().getNote(maxGuid, true, false,
-					false, false, true);
-			boolean isNoteActive = false;
-			if(maxNote != null) {
-				isNoteActive = maxNote.isActive();
-			}
-			
-			// 存在していて、かつ関連度0でなければノート情報を取得して連想ノートリストに追加
-			if (isNoteActive && maxNum > 0) {
-				QListWidgetItem item = new QListWidgetItem();
-				RensoNoteListItem myItem = new RensoNoteListItem(maxNote, maxNum, conn);
-				item.setSizeHint(new QSize(0, 90));
-				this.addItem(item);
-				this.setItemWidget(item, myItem);
-				rensoNoteListItems.put(item, maxGuid);
+				
+				// 次の最大値探索で邪魔なので最大値をHashMapから削除
+				copyHistory.remove(maxGuid);
+	
+				// 関連度最大のノートがアクティブか確認
+				Note maxNote = conn.getNoteTable().getNote(maxGuid, true, false,
+						false, false, true);
+				boolean isNoteActive = false;
+				if(maxNote != null) {
+					isNoteActive = maxNote.isActive();
+				}
+				
+				// 存在していて、かつ関連度0でなければノート情報を取得して連想ノートリストに追加
+				if (isNoteActive && maxNum > 0) {
+					QListWidgetItem item = new QListWidgetItem();
+					RensoNoteListItem myItem = new RensoNoteListItem(maxNote, maxNum, conn);
+					item.setSizeHint(new QSize(0, 90));
+					this.addItem(item);
+					this.setItemWidget(item, myItem);
+					rensoNoteListItems.put(item, maxGuid);
+				}
+			} else {
+				break;
 			}
 		}
 	}
